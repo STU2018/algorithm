5f2879ffd289f65bcaaefb2124f07997de03b3bb
ljh <ljh2024@foxmail.com>
2022-12-16 23:38:23 +0800

    implement selection sort

---
+++ warehouse-01/selection_sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void selectSort(int arr[], int n) {
+    int minIndex;
+    for (int i = 0; i < n - 1; i++) {
+        minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i) {
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+}
+
+int main() {
+    int arr[] = {3, 5, 2, 8, 1, 9, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    selectSort(arr, n);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
