1986bd0175d156b9b4bf8df7d0be9272e1d287f4
ljh <ljh2024@foxmail.com>
2022-12-16 23:40:10 +0800

    实现选择排序

--- warehouse-01/selection_sort.cpp
+++ warehouse-01/selection_sort.cpp
@@ -21,7 +21,7 @@ void selectSort(int arr[], int n) {
 
 int main() {
     int arr[] = {3, 5, 2, 8, 1, 9, 4};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n = 7;
     selectSort(arr, n);
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
