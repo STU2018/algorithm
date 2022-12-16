2ec7b3cc99b36a17f5d7ddf91cc32f384995a90c
ljh <ljh2024@foxmail.com>
2022-12-16 23:49:06 +0800

    实现冒泡排序

---
+++ warehouse-01/bubble_sort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {3, 5, 1, 8, 4, 9, 6, 2, 7};
+    int n = 9;
+    bubbleSort(arr, n);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
